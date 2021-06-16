4e8527eab5daf6881a79daa42da4ffad9c7b0431
Anusha604 <85864046+Anusha604@users.noreply.github.com>
2021-06-16 15:49:20 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,20 @@
+package pattern;
+
+public class Pattern3 {
+	public static void main(String[] args){
+		
+		for (int i = 1; i<=5; i++)//5 ROWS(RIGHT)
+		{
+			for (int j = 1; j<=5; j++)// 5 COLOUMS(LINE)
+			{
+				System.out.print(j);
+			}
+		    System.out.println();
+		}
+
+		}}
+
+
+
+
+
