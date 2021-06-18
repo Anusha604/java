26bddc826f34b17b61870e686602d24ac8b620f9
Anusha604 <85864046+Anusha604@users.noreply.github.com>
2021-06-18 17:48:56 +0530

    Add files via upload

---
+++ Pattern11.java
@@ -0,0 +1,16 @@
+package patterns;
+
+public class Pattern11 {
+	public static void main(String[] args)
+	{
+		for (char i = 'E'; i >= 'A'; i--)
+		{
+			for(int j = 1; j <= 5; j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
